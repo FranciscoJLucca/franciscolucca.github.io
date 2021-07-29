a7d179fab9539049925c215d8070214bb87edbbf
Francisco Lucca <francisco.lucca@pucrs.br>
2021-07-29 00:01:11 -0300

    Start skills development

---
+++ components/Skills.js
@@ -0,0 +1,12 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+const Skills = () => {
+  return (
+    <Flex width="100vw" height="100vh" bg="gray.400" justify="center">
+      <Heading pt="10px">Skills</Heading>
+      
+    </Flex>
+  );
+}
+
+export default Skills;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import { VStack } from "@chakra-ui/react";
 import About from "../components/About";
 import Header from "../components/Header";
+import Skills from "../components/Skills";
 
 const Index = () => (
-  <About />
+  <>
+    <About />
+    <Skills />
+  </>
 );
 
 export default Index;
